1a87c9350a07cfe35634a25f04a4c95d1cc88b7a
Shwetik <41204318+Shwetik@users.noreply.github.com>
2021-12-06 10:10:40 +0530

    Delete team directory

--- team/src/main/kotlin/repository/ISprintRepo.kt
+++
@@ -1,9 +0,0 @@
-package com.axis.team.repository
-
-import com.axis.team.entity.Sprint
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ISprintRepo : MongoRepository<Sprint, String> {
-}
